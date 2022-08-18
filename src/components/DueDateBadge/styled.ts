5208d5da5bcff7d818298141daf6c7d1bcd06d66
motionman <dmytro.hutsulenko@ekreative.com>
2022-08-18 15:18:01 +0300

    fix: badge align

--- src/components/DueDateBadge/styled.ts
+++ src/components/DueDateBadge/styled.ts
@@ -7,8 +7,9 @@ export const DueDateContainer = styled.View`
 export const OverdueDaysContainer = styled.View<{ isOverdue?: boolean }>`
   min-width: 20px;
   min-height: 20px;
-  justify-content: center;
   border-radius: 50px;
+  align-items: center;
+  justify-content: center;
   background-color: ${({ isOverdue }) => (isOverdue ? '#FF5757' : '#FFB938')};
 `;
 
